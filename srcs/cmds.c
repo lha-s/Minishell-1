b27265cabc509ca27803321afef16c46f2808fc2
Mulham Soufi <musoufi@z4r10p1.42lyon.fr>
2021-07-20 18:17:26 +0200

    .

--- srcs/cmds.c
+++ srcs/cmds.c
@@ -6,7 +6,7 @@
 /*   By: musoufi <musoufi@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/27 21:21:41 by musoufi           #+#    #+#             */
-/*   Updated: 2021/07/19 18:53:17 by musoufi          ###   ########lyon.fr   */
+/*   Updated: 2021/07/20 18:17:05 by musoufi          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,7 +58,12 @@ void	exec_cmd(t_token *token, char **env)
 		tmp = ft_strjoin(tmp, ft_strnstr(token->cmd, cmd[0], ft_strlen(cmd[0])));
 		if (stat(tmp, &buf) == 0)
 		{
-			fprintf(stderr, "cmd=%s | out=%d\n", token->cmd, token->out);
+			if (strncmp(token->cmd, "wc", 2) == 0)
+			{
+				fprintf(stderr, "lol");
+				close(token->fd[0]); //le programme termine pas, tenter de close
+			}
+			//fprintf(stderr, "cmd=%s | out=%d\n", token->cmd, token->out);
 			execve(tmp, cmd, env);
 		}
 		i++;
@@ -88,8 +93,7 @@ void	 cfg_piping(t_token *token, pid_t pid, int old_in)
 			dup2(old_in, STDIN_FILENO);
 			close(old_in);
 		}
-		else
-			close(token->fd[0]);
+		close(token->fd[0]);
 	}
 }
 
@@ -112,22 +116,32 @@ int fork_process(t_token *token, char **env)
 	int wret;
 	
 	wret = 0;
-	old_in = token->fd[0];
 	if (ft_strncmp(token->cmd, "exit", 4) == 0)
 		return (write_exit());
-	if (token->out && pipe(token->fd) < 0)
+	if (pipe(token->fd) < 0)
 	{
 		printf("Error: pipe failed\n");
 		return (FALSE);
 	}
+	old_in = token->fd[0];
+	fprintf(stderr, "old_in2 = %d\n", old_in);
+	/*
+	Parent:
+	si (out == 1)
+		connecter l'entree du pipe (0) à la sortie stdout (1)
+	fils:
+	si (in == 1)
+		connecter la sortie du pipe (1) à l'entrée stdin (0)
+	*/
 	pid_t pid = fork();
 	if (pid == 0)
 	{
-		if (token->in)
-			dup2(token->fd[0], STDOUT_FILENO);
-		if (token->out)
-			
-		//cfg_piping(token, pid, old_in);
+		char buf;
+		if (strncmp(token->cmd, "wc", 3) == 0)
+			//fscanf(stdin, "%s\n", &buf);
+		fprintf(stderr, "WC=%s\n", &buf);
+		fprintf(stderr, "%s fd0= %d, fd1= %d\n", token->cmd, token->fd[0], token->fd[1]);
+		cfg_piping(token, pid, old_in);
 		recursive_process(token, env);
 		write_output("Error: fork had to be forced to quit\n");
 		exit(0);
@@ -141,8 +155,7 @@ int fork_process(t_token *token, char **env)
 			wret = WEXITSTATUS(wstatus);
 		else if (WIFSIGNALED(wstatus))
 			wret =  128 + WTERMSIG(wstatus);
-		printf("EXIT STATUS=%d\n", wret);
-		//wait(&pid);
+		//printf("EXIT STATUS=%d\n", wret);
 	}
 	else
 		write_output("Error: fork failed, command failed\n");
